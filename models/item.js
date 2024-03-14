437aea79d52e4362167260734747e812dc63a316
Vigneshkumar P <vigneshkumar.p@fortunesoftit.com>
2024-03-14 20:33:10 +0530

    api added for auction

---
+++ models/item.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    startingPrice: {
+        type: Number,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    highestBid: {
+        type: Number,
+        default: 0
+    },
+    winningBidder: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Item', itemSchema);
